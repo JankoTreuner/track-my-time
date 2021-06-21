a6fa4db1f826c1e8ace8af202f3f73b970a6a7d5
Janko Treuner <git@jankotreuner.de>
2021-06-21 12:03:38 +0200

    Added another (dummy) unittest to test ci-workflow

--- trackmytime/timetracker/tests.py
+++ trackmytime/timetracker/tests.py
@@ -9,3 +9,7 @@ class TimeEntryTestCase(TestCase):
     def test_timeentry_exists(self):
         entry = TimeEntry.objects.all()
         self.assertEqual(1, entry.count())
+
+    def test_timeentry_active(self):
+        entry = TimeEntry.objects.first()
+        self.assertEqual(True, entry.is_active) 
